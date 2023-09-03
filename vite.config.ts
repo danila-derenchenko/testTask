fc3fb2b1a3fc823e482e4fb041e6d74f58a9e0c8
danila-derenchenko <danila.derenchenko@gmail.com>
2023-09-03 13:27:19 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginGhPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/testTask/',
+  base: '/testTask',
   plugins: [
-    react(),
-    VitePluginGhPages()
+    react()
   ],
 })
